6f79c93fcfea7aa219f9fd853500021d221c1b1f
TOMAZ FERREIRA GOMES JUNIOR <tomaz.gomes@hotmail.com>
2021-08-21 17:27:17 -0300

    mudanca na logo

--- javascript.js
+++ javascript.js
@@ -1,8 +1,1 @@
-const botao = document.querySelector('botao');
-
-function ativarMenu() {
-    const nav = document.querySelector('botao');
-    nav.classList.toggle('active');
-}
-
-botao.addEventListener('click', ativarMenu);
+document.querySelector('.logo')
